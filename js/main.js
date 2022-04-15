b424046ddebe4000ac39a456899738be2ddc7e4c
dalton05 <dalton05@gmail.com>
2022-04-14 18:27:03 -0600

    more functionality... should work with healing spells now

--- js/main.js
+++ js/main.js
@@ -27,12 +27,15 @@ function handleClick(click) {
 function handleSpellsObj(obj, str) {
     console.log(obj)
     document.querySelector('.flex-child:first-child').style = 'border-right: 2px solid black;'
-    document.querySelector(`#descTitle${str}`).innerText = obj.name
-    document.querySelector(`#type${str}`).innerText = `Attack Type: ${obj.attack_type}`
-    document.querySelector(`#castingTime${str}`).innerText = `Casting Time: ${obj.casting_time}`
-    document.querySelector(`#range${str}`).innerText = `Range: ${obj.range}`
-    document.querySelector(`#damage${str}`).innerText = obj.damage.damage_at_slot_level ? `Damage: ${obj.damage.damage_at_slot_level['1']}` : `Damage: ${obj.damage.damage_at_character_level['1']}`
-    document.querySelector(`#damageType${str}`).innerText = `Damage Type: ${obj.damage.damage_type.name}`
+    document.querySelector(`#descTitle${str}`).innerText = obj.name ? obj.name : 'Not Found'
+    document.querySelector(`#type${str}`).innerText = obj.school.name ? `Type: ${obj.school.name}` : 'none'
+    document.querySelector(`#castingTime${str}`).innerText = obj.casting_time ? `Casting Time: ${obj.casting_time}` : 'not defined'
+    document.querySelector(`#range${str}`).innerText = obj.range ? `Range: ${obj.range}` : 'not defined'
+    if (obj.damage) {
+        document.querySelector(`#damage${str}`).innerText = obj.damage.damage_at_slot_level ? `Damage: ${obj.damage.damage_at_slot_level['1']}` : `not defined`
+        document.querySelector(`#damage${str}`).innerText = obj.damage.damage_at_character_level ? `Damage: ${obj.damage.damage_at_character_level['1']}` : 'not defined'
+    }
+    document.querySelector(`#damageType${str}`).innerText = obj.damage ? `Damage Type: ${obj.damage.damage_type.name}` : null
     document.querySelector(`#duration${str}`).innerText = `Duration: ${obj.duration}`
     document.querySelector(`#classes${str}`).innerText = `Available To: ${obj.classes.map(e => e.name).join(', ')}`
     document.querySelector(`#description${str}`).innerText = `Description: ${obj.desc.join('\n \n')} \n \n ${obj.higher_level.join(', ')}`
